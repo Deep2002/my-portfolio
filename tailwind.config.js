4dfc8cb83c47de36461ca17214de1bc835783e72
Deep Parmar <deepsmac@Deeps-MacBook-Pro.local>
2023-10-24 12:33:45 -0500

    Added fonts to the tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        impS: ["Imperial Script", "sans-serif"],
+        inconsolata: ["Inconsolata", "sans-serif"],
+      },
+    },
+    screens: {
+      tablet: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      laptop: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      desktop: "1280px",
+      // => @media (min-width: 1280px) { ... }
+    },
   },
   plugins: [],
 };
